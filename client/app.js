c8073cc0036634ee0ec64f2d965d530cc4209524
Zack <Zackcolemanmusic@gmail.com>
2018-01-05 14:08:37 +0000

    Added modal box with start button

--- client/app.js
+++ client/app.js
@@ -1,9 +1,23 @@
 const MapWrapper = require('./MapWrapper');
+const Modal = require('./Modal');
 let countryMap;
 
 const app = function() {
   console.log('App started');
   initialize(48.21, 16.37);
+  const modal = new Modal({
+    title: "Where on Earth?",
+    body: "The ultimate Country and Capital's game!",
+    buttons: {
+      action: {
+        label: "Start Game",
+        fn: function(){
+          modal.hide();
+        }
+      }
+    }
+  });
+  modal.show();
 };
 
 const initialize = function(lat, lng) {
@@ -11,6 +25,8 @@ const initialize = function(lat, lng) {
   let mapDiv = document.getElementById('map');
   countryMap = new MapWrapper(mapDiv, center, 5);
 };
+
+
 
 
 document.addEventListener('DOMContentLoaded', app);
