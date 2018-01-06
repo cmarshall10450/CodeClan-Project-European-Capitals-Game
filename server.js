8c709a86ec75bc7556f774fdd35849b424a89c17
Chris Marshall <cmarshall10450@gmail.com>
2018-01-06 12:50:22 +0000

    Added route to save scores to db

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
 const PORT = 5000;
@@ -8,6 +9,8 @@ const MONGO_URL = 'mongodb://localhost:27017/';
 const app = express();
 
 app.use(cors('*'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static('./build'));
 
@@ -50,6 +53,20 @@ MongoClient.connect(MONGO_URL, function(err, client) {
 
         res.json(randomCountry);
       });
+  });
+
+  app.post('/api/scores', function(req, res) {
+    db.collection('wishlist').insert(req.body, function(err, result) {
+      if (err) {
+        console.log(err);
+        res.status(500);
+        res.send();
+        return;
+      }
+
+      res.status(201);
+      res.json(result.ops[0]);
+    });
   });
 
   console.log('Connected to DB');
