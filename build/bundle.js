1dec9ce958b2a6d60d3c98c84a76034c13e5cbde
Zack <Zackcolemanmusic@gmail.com>
2018-01-08 15:47:45 +0000

    Reverted double click

--- build/bundle.js
+++ build/bundle.js
@@ -279,7 +279,7 @@ const MapWrapper = function(container, coordinates, zoom, callback) {
 
       google.maps.event.addListener(
         this.googleMap,
-        'dblclick',
+        'click',
         function(event) {
           if (this.marker) {
             this.marker.setPosition(event.latLng);
--- client/models/MapWrapper.js
+++ client/models/MapWrapper.js
@@ -24,7 +24,7 @@ const MapWrapper = function(container, coordinates, zoom, callback) {
 
       google.maps.event.addListener(
         this.googleMap,
-        'dblclick',
+        'click',
         function(event) {
           if (this.marker) {
             this.marker.setPosition(event.latLng);
