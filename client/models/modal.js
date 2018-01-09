5e3ac3630bfcf7f6fd884d205f822307510b1dbf
Kynan <k.song@blueyonder.co.uk>
2018-01-09 11:51:56 +0000

    Refactored modal load screen.

--- client/models/modal.js
+++ client/models/modal.js
@@ -3,41 +3,75 @@ const Modal = function(options) {
   this.visible = false;
 };
 Modal.prototype.build = function(data) {
+  const modalWrapper = document.createElement('div');
+  // let html = '';
+  // html += `
+  //   <div class="overlay"></div>
+  //   <div class="modal">
+  //     <div class="modal-close">x</div>
+  //     <div class="modal-header">
+  //       <h1>${data.title}</h1>
+  //     </div>
+  //     <hr/>
+  //     <div class="modal-body">
+  //       ${data.body}
+  //     </div>
+  // `;
+
+  const overlay =  document.createElement('div');
+  overlay.classList.add('overlay');
+  modalWrapper.appendChild(overlay);
+
   const modal = document.createElement('div');
-  let html = '';
-  html += `
-    <div class="overlay"></div>
-    <div class="modal">
-      <div class="modal-close">x</div>
-      <div class="modal-header">
-        <h1>${data.title}</h1>
-      </div>
-      <hr/>
-      <div class="modal-body">
-        ${data.body}
-      </div>
-  `;
-  if (this.options.buttons) {
-    html += `<div class="modal-buttons">`;
-    if (this.options.buttons.close) {
-      html += `
-        <button class="modal-button btn-close">
-          ${this.options.buttons.close.label}
-        </button>`;
-    }
-    if (this.options.buttons.action) {
-      html += `
-        <button class="modal-button btn-action">
-          ${this.options.buttons.action.label}
-        </button>`;
-    }
-    html += `</div>`;
-  }
-  html += `</div>`;
-  modal.classList.add('modal-wrapper');
-  modal.innerHTML = html;
-  return modal;
+  modal.classList.add('modal');
+
+  const modalClose = document.createElement('div');
+  modalClose.classList.add('modal-close');
+  modal.appendChild(modalClose);
+
+  const modalHeader = document.createElement('div');
+  modalHeader.classList.add('modal-header');
+  modal.appendChild(modalHeader);
+
+  const title = document.createElement('h1');
+  title.innerText = data.title;
+  modalHeader.appendChild(title);
+
+  const line = document.createElement('hr');
+  modal.appendChild(line);
+
+  const modalBody = document.createElement('div');
+  modalBody.classList.add('modal-body');
+  modalBody.innerHTML = data.body;
+  modal.appendChild(modalBody);
+
+  modalWrapper.appendChild(modal);
+
+
+
+//   if (this.options.buttons) {
+//     html += `<div class="modal-buttons">`;
+//     if (this.options.buttons.close) {
+//       html += `
+//         <button class="modal-button btn-close">
+//           ${this.options.buttons.close.label}
+//         </button>`;
+//     }
+//     if (this.options.buttons.action) {
+//       html += `
+//         <button class="modal-button btn-action">
+//           ${this.options.buttons.action.label}
+//         </button>`;
+//     }
+//     html += `</div>`;
+//   }
+//   html += `</div>`;
+//   modal.classList.add('modal-wrapper');
+//   modal.innerHTML = html;
+//   return modal;
+return modalWrapper
 };
+
 Modal.prototype.show = function() {
   if (!this.visible) {
     document.querySelector('body').appendChild(this.build(this.options));
